bd7668d619189e1a6a2d0d22917bcbe62c6d883a
Hasin Hayder <countdraculla@gmail.com>
2024-09-23 12:20:00 +0530

    Tailwind Config JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,20 +7,6 @@
  * Version: 1.2
  */
 
-tailwind.config = {
-    theme: {
-        extend: {
-            colors: {
-                primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a", "950": "#172554" }
-            },
-            fontSize: {
-                '10xl': '10rem',
-                '11xl': '12rem',
-                '12xl': '14rem',
-            }
-        }
-    }
-}
 function clockApp() {
     return {
         currentTime: '00:00:00',
